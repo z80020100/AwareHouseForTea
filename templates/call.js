56e5c42cafa94fd36df230e335c4b7c1201be26b
Jy-Liu <b03502040@ntu.edu.tw>
2016-10-04 18:15:27 +0800

    change the format of call page

--- templates/call.js
+++ templates/call.js
@@ -1,20 +1,8 @@
 $(document).ready(function(){
-  $("select").on('click', function() {
-    if (this.value == -1) {
-      $('select[name=item]').css('display', 'none');
-      $('input[name=item]').css('display', 'inline-block');
-    }
-  });
-
-  $('input[type=submit]').on('click', function() {
-    var item1 = $('select[name=item] option:selected').val();
-    var item2 = $.trim($('input[name=item]').val());
-    var num = $.trim($('input[name=number]').val());
-    if(((item1 && item1 != -1) || item2 != '') && $.isNumeric(num)) {
+  $('#submit').on('click', function() {
+    $('input:checked').each(function() {
+      console.log($(this).parent().parent().find('input[type=number]').data('name'));
       alert('已經叫料');
-    }
-    else {
-      alert('資料有誤');
-    }
+    });
   });
 });
