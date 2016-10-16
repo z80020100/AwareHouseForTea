611ee8aedb1f9c8061f11a06a700dbf1cfc417a1
Jy-Liu <b03502040@ntu.edu.tw>
2016-10-16 19:40:23 +0800

    time reverse sort in call page

--- templates/call.js
+++ templates/call.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#submit').on('click', function() {
     $('input:checked').each(function() {
       console.log($(this).parent().parent().find('input[type=number]').data('name'));
-      alert('已經叫料');
     });
+    alert('已經叫料');
   });
 });
