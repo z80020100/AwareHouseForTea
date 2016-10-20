381138b77b6f9d9871455ab4cb22b0f2ac02c054
Yist Lin <yishen992@gmail.com>
2016-10-20 20:22:10 +0800

    testing listorder.php

--- listorder.php
+++ listorder.php
@@ -1,7 +1,7 @@
 <?php
 
-//require_once('includes/general.php');
-//header("Content-Type:text/html; charset=utf-8");
+// require_once('includes/general.php');
+// header("Content-Type:text/html; charset=utf-8");
 
 $_PAGE_TITLE = '即時訂單';
 require_once('includes/header.php');
@@ -11,19 +11,18 @@ not_staff_redirect();
 
 $template = $twig->loadTemplate('listorder.html');
 
-/*
-$sql = "SELECT * FROM `orders` ORDER BY `orders`.`o_time` DESC where `orders`.`status` = WAIT";
-$result = $db->query($sql);
-$num = $db->numrow($result);
-$all_orders = array();
-*/
-
-not_staff_redirect();
+// $sql = "SELECT * FROM `orders` WHERE `orders`.`status` = 'WAIT' ORDER BY `orders`.`o_time` DESC";
+// $result = $db->query($sql);
+// $all_orders = array();
+// if ($result) {
+// 	while ($row = $result->fetch_assoc()) {
+// 		$all_orders[] = $row;
+// 	}
+// }
 
 $_HTML .= $template->render(array(
-	
-));
 
+));
 
 require_once('includes/footer.php');
 
