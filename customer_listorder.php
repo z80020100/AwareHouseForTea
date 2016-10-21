8555fb10ed982dd665ba28d3bb7a535d38fee271
JCly-rikiu <jk123456789jp@gmail.com>
2016-10-21 09:50:29 +0800

    append shop_id at all url in header()

--- customer_listorder.php
+++ customer_listorder.php
@@ -5,7 +5,7 @@ require_once('includes/header.php');
 
 not_login_redirect();
 if(!checkAuth(AUCUSTOMER|AUSTAFF|AUADMIN)){
-		header("location:register.php");
+		header("location:register.php?shop_id=" . $_shopID);
 		die('');
 }
 
@@ -23,20 +23,20 @@ $all_orders = array();
 
 	$sql = "SELECT * FROM `config` WHERE `name` = 'verification_time'";
 	$Qver_t = $db->query_select_one($sql);
-	
+
 	if( $Qver_t['value']  < time() ){
 		$sql = "UPDATE `config` SET `value` = '".(time()+30)."' WHERE `config`.`name` = 'verification_time'";
 		$db->query($sql);
-		
+
 		$new_hash = substr( hash("sha256", $Qver['value']) , 0, 4);
-		
+
 		$sql = "UPDATE `config` SET `value` = '".$new_hash."' WHERE `config`.`name` = 'verification' ";
 		$db->query($sql);
-		
+
 		$Qver['value'] = $new_hash;
 	}
 */
-	
+
 $_HTML .= $template->render(array(
 	//'verification_code' => $Qver['value'],
 ));
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ header("Content-Type:text/html; charset=utf-8");
 
 not_login_redirect();
 if(!checkAuth(AUCUSTOMER|AUSTAFF|AUADMIN)){
-		header("location:register.php");
+		header("location:register.php?shop_id=" . $_shopID);
 		die();
 }
 
@@ -21,7 +21,7 @@ $all_series = array();
 while($series_data = $db->fetch_array($result)){
 	//echo '<h3>'. $series_data['name'] . '</h3>';
 	$snum = array_push( $all_series, $series_data )-1;
-	
+
 	// List the dishes in the series
 	$sql = "SELECT * FROM `main` WHERE `s_id` = ". $series_data['s_id'] ." ORDER BY `main`.`order_num` ASC";
 	$m_result = $db->query($sql);
@@ -30,26 +30,26 @@ while($series_data = $db->fetch_array($result)){
 		$mnum = array_push($all_main, $main_data)-1;
 		//echo '<li>';
 		//echo 'm_id('.$main_data['m_id'].'):'.'name('.$main_data['name'].'):'.'price('.$main_data['price'].'):'.'at_id('.$main_data['at_id'].'):';
-		
+
 		//$sql = "SELECT * FROM `additional_type` WHERE `at_id` = ". $main_data['at_id'] ;
 		//$at_data = $db->query_select_one($sql);   // Fetch directly : only one additional_type per main, no need to loop
-		
+
 		//echo "additional_item(";
 		// List the "Additional options" that are directly related to the dish
 		$sql = "SELECT * FROM  `additional_item` WHERE `at_id` = ".$main_data['at_id'] ;
 		$ai_result = $db->query($sql);
 		$all_ai = array();
 		while($ai_data = $db->fetch_array($ai_result)){
-		//	echo "<i>". $ai_data['name'] ."</i>,";			
+		//	echo "<i>". $ai_data['name'] ."</i>,";
 			$ainum = array_push($all_ai, $ai_data)-1;
 		}
 		$all_main[$mnum]['ai'] = $all_ai;
 		//echo ")";
-		
-		
+
+
 		$all_ro = array();
 		if($main_data['required_option'] == true){
-			// 1 dish <--> several required options 
+			// 1 dish <--> several required options
 			$sql = "SELECT * FROM `required_option` WHERE `m_id` = ".$main_data['m_id'];
 			$ro_result = $db->query($sql);
 			$all_ro = array();
@@ -63,19 +63,19 @@ while($series_data = $db->fetch_array($result)){
 				$all_ro_ai = array();
 				while($ai_data = $db->fetch_array($ai_result)){
 					$ro_ainum = array_push($all_ro_ai, $ai_data)-1;
-					//echo "<i>". $ai_data['name'] ."</i>,";			
+					//echo "<i>". $ai_data['name'] ."</i>,";
 				}
 				$all_ro[$ronum]['ai'] = $all_ro_ai;
-				//echo ")";				
+				//echo ")";
 			}
 		}
 		else{
 			//echo "( noro )";
 		}
 		$all_main[$mnum]['ro'] = $all_ro;
-		
+
 		//echo '</li>';
-	}	
+	}
 	$all_series[$snum]['main'] = $all_main;
 }
 
@@ -94,8 +94,8 @@ else if($_AWMode == "BUSINESS")
     $template = $twig->loadTemplate('customer_menu_business.html');
 
 
-	
-	
+
+
 
 
 $_HTML .= $template->render(array(
@@ -111,4 +111,3 @@ require_once('includes/footer.php');
 
 
 ?>
-
