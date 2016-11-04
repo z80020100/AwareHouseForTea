84985563a685483bc0559d42b20b930585bdf262
Yist Lin <yishen992@gmail.com>
2016-11-05 00:11:35 +0800

    2 tabs in sms_promote

--- sms_promote.php
+++ sms_promote.php
@@ -4,42 +4,52 @@
 $_PAGE_TITLE = '簡訊推播';
 require_once('includes/header.php');
 
-$template = $twig->loadTemplate('sms_promote.html');
-
 not_admin_redirect();
 
-$info = array();
-
-// only query customer
-// query u_id, ui_phone
-$sql = "SELECT `user`.`u_id`, `user_info`.`ui_phone` FROM `user`, `user_info` WHERE `user`.`u_id` = `user_info`.`u_id` AND `user`.`u_type` = 1";
-foreach ($db->query($sql) as $row) {
+if (isset($_POST['sms_mode'])) {
+    $info = array();
     
-    // only query current shop_id
-    // query table `log` for quantity, price
-    $total_price = 0;
-    $sql = "SELECT `quantity`,`price` FROM `log` WHERE `u_id` = " . $row['u_id'] . " AND `shop_id` = " . $_SESSION['GET_shop_id'];
-    foreach ($db->query($sql) as $log) {
-        $total_price += intval($log['quantity']) * intval($log['price']);
+    // sms_mode, 0 for customer, 1 for shop
+    if ($_POST['sms_mode'] == 0) {
+        // only query customer
+        // query u_id, ui_phone
+        $sql = "SELECT `user`.`u_id`, `user_info`.`ui_phone` FROM `user`, `user_info` WHERE `user`.`u_id` = `user_info`.`u_id` AND `user`.`u_type` = 1";
+        foreach ($db->query($sql) as $row) {
+            
+            // only query current shop_id
+            // query table `log` for quantity, price
+            $total_price = 0;
+            $sql = "SELECT `quantity`,`price` FROM `log` WHERE `u_id` = " . $row['u_id'] . " AND `shop_id` = " . $_SESSION['GET_shop_id'];
+            foreach ($db->query($sql) as $log) {
+                $total_price += intval($log['quantity']) * intval($log['price']);
+            }
+            $row['price'] = $total_price;
+
+            // only query current shop_id
+            // query number of rows in table `orders`
+            $total_times = 0;
+            $sql = "SELECT * FROM `orders` WHERE `u_id` = " . $row['u_id'] . " AND `shop_id` = " . $_SESSION['GET_shop_id'];
+            $result = $db->query($sql);
+            if ($result)
+                $total_times = $db->numrow($result);
+            $row['times'] = $total_times;
+
+            $info[] = $row;
+        }
     }
-    $row['price'] = $total_price;
-
-    // only query current shop_id
-    // query number of rows in table `orders`
-    $total_times = 0;
-    $sql = "SELECT * FROM `orders` WHERE `u_id` = " . $row['u_id'] . " AND `shop_id` = " . $_SESSION['GET_shop_id'];
-    $result = $db->query($sql);
-    if ($result)
-        $total_times = $db->numrow($result);
-    $row['times'] = $total_times;
-
-    $info[] = $row;
+    else if ($_POST['sms_mode'] == 1) {
+        $sql = "SELECT `shop_id`,`shop_name`,`shop_owner` FROM `shop`";
+        foreach ($db->query($sql) as $shop) {
+            $info[] = $shop;
+        }
+    }
+    echo json_encode($info);
+    exit();
 }
 
-$_HTML .= $template->render(array(
-    'num_info' => count($info),
-	'info' => $info
-));
+$template = $twig->loadTemplate('sms_promote.html');
+
+$_HTML .= $template->render(array());
 
 require_once('includes/footer.php');
 
--- templates/sms_promote.js
+++ templates/sms_promote.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#promote_table").tablesorter();
     $("#promote_table").tablesorter({ sortList: [[0,0], [1,0]] });
-    // add_table_row("0912123123","2016/07/04","11","10000","abcedd","12");
+    // add_customer_row("0912123123","11","10000","12");
 
     $("#sms_button").click(function(){
        $("#sms_content").css({"display":"block"});
@@ -10,32 +10,106 @@ $(document).ready(function(){
        $("#sms_content").css({"display":"none"});
     });
 
-    $("#select_all").on("click", function() {
-        if(this.checked){
-            $('.sms_checkbox').each(function () {
-                this.checked = true;
-            });
-        }
-        else{
-            $('.sms_checkbox').each(function () {
-                this.checked = false;
-            });
-            
-        }
+    // mode selector
+    $(".tab").on("click", function() {
+        $(this).addClass('active');
+        $(this).siblings('div.tab').removeClass('active');
+        $('#promote_table').empty().append('<tbody><tr><td colspan="5">請點選搜尋</td></tr></tbody>');
+    });
+
+    // search results
+    $("#search_button").on("click", function() {
+        $.ajax({
+            url:"sms_promote.php",
+            method: "POST",
+            dataType: "json",
+            data: {"sms_mode": $('.tab.active').attr('sms-mode')}, // sms_mode, 0 for customer, 1 for shop
+            async: false
+        })
+        .done(function(msg) {
+            handle_query(msg);
+        })
+        .fail(function() {
+            alert('query failed!');
+        });
     });
 });
 
-function add_table_row(phone_number, order_time, order_num, amount, content, sms_num){
+function add_customer_row(phone_number, order_num, amount, sms_num) {
     $('table tbody').append(
         $('<tr>').append(
             $('<td>').html("<input class=\"w3-check sms_checkbox\" type=\"checkbox\">"),
             $('<td>').text(phone_number),
-            $('<td>').text(order_time),
             $('<td>').text(order_num),
             $('<td>').text(amount),
-            $('<td>').text(content),
             $('<td>').text(sms_num)
         )
     );
     return;
+}
+
+function add_shop_row(shopid, shopname, shopowner) {
+    $('table tbody').append(
+        $('<tr>').append(
+            $('<td>').html("<input class=\"w3-check sms_checkbox\" type=\"checkbox\">"),
+            $('<td>').text(shopid),
+            $('<td>').text(shopname),
+            $('<td>').text(shopowner)
+        )
+    );
+    return;
+}
+
+function handle_query(msg) {
+    
+    // sms_mode, 0 for customer, 1 for shop
+    var sms_mode = $('.tab.active').attr('sms-mode');
+
+    if (sms_mode == 0) {
+        $('table').empty().append(
+            $('<thead>').append(
+                $('<tr>').append(
+                    $('<td rowspan="2">').append(
+                        '全選 <input class="w3-check sms_checkbox" type="checkbox" id="select_all">'
+                    ),
+                    $('<td rowspan="2">').text('號碼'),
+                    $('<td colspan="2">').text('統計報表'),
+                    $('<td rowspan="2">').text('已推播簡訊')
+                ),
+                $('<tr>').append(
+                    $('<td>').text('消費次數'),
+                    $('<td>').text('累計消費金額')
+                )
+            ),
+            $('<tbody>')
+        );
+        for (var i = 0; i < msg.length; i++) {
+            add_customer_row(msg[i]['ui_phone'], msg[i]['times'], msg[i]['price'],0);
+        }
+    }
+    else if (sms_mode == 1) {
+        $('table').empty().append(
+            $('<thead>').append(
+                $('<tr>').append(
+                    $('<td>').append(
+                        '全選 <input class="w3-check sms_checkbox" type="checkbox" id="select_all">'
+                    ),
+                    $('<td>').text('分店 ID'),
+                    $('<td>').text('分店名稱'),
+                    $('<td>').text('分店老闆')
+                )
+            ),
+            $('<tbody>')
+        );
+        for (var i = 0; i < msg.length; i++) {
+            add_shop_row(msg[i]['shop_id'], msg[i]['shop_name'], msg[i]['shop_owner']);
+        }
+    }
+
+    $("#select_all").on("click", function() {
+        var checkstatus = this.checked;
+        $('.sms_checkbox').each(function () {
+            this.checked = checkstatus;
+        });
+    });
 }
