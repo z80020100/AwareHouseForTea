4783f998a9f971c9b68aec7443630fe3ffff3efa
Yist Lin <yishen992@gmail.com>
2016-11-04 11:10:10 +0800

    refactor sms_promote.html and sms_promote.js

--- templates/sms_promote.js
+++ templates/sms_promote.js
@@ -1,47 +1,33 @@
-
 $(document).ready(function(){
-
-
     $("#promote_table").tablesorter();
     $("#promote_table").tablesorter({ sortList: [[0,0], [1,0]] });
     add_table_row("0912123123","2016/07/04","11","10000","abcedd","12");
 
-
     $("#sms_button").click(function(){
        $("#sms_content").css({"display":"block"});
-
     });
     $("#close_sms_content").click(function(){
        $("#sms_content").css({"display":"none"});
-
     });
 
-
-     $("#select_all").on("click", function() {
-            if(this.checked){
-                $('.sms_checkbox').each(function () {
-                    this.checked = true;
-                });
-            }
-            else{
-                $('.sms_checkbox').each(function () {
-                    this.checked = false;
-                });
-                
-            }
-     });
-
-
-
+    $("#select_all").on("click", function() {
+        if(this.checked){
+            $('.sms_checkbox').each(function () {
+                this.checked = true;
+            });
+        }
+        else{
+            $('.sms_checkbox').each(function () {
+                this.checked = false;
+            });
+            
+        }
+    });
 });
 
-
-
 function add_table_row(phone_number, order_time, order_num, amount, content, sms_num){
-    var tr_temp = $('<tr>');
-
     $('table tbody').append(
-        tr_temp.append(
+        $('<tr>').append(
             $('<td>').html("<input class=\"w3-check sms_checkbox\" type=\"checkbox\">"),
             $('<td>').text(phone_number),
             $('<td>').text(order_time),
@@ -51,10 +37,5 @@ function add_table_row(phone_number, order_time, order_num, amount, content, sms
             $('<td>').text(sms_num)
         )
     );
-
     return;
 }
-
-
-
-
