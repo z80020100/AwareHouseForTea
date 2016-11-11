c58d493b3553ee7a3c0650da3a26a78f3c011021
Yist Lin <yishen992@gmail.com>
2016-11-11 12:12:06 +0800

    add filter on page sms_promote

--- templates/sms_promote.js
+++ templates/sms_promote.js
@@ -20,11 +20,35 @@ $(document).ready(function(){
 
     // query results
     $("#search_button").on("click", function() {
+        var sms_mode = $('.tab.active').attr('sms-mode');
+        var starttime = new Date( $('input[name=filter1]').val() ).getTime() / 1000;
+        var endtime = new Date( $('input[name=filter2]').val() ).getTime() / 1000;
+        var baseline = $('select[name=filter3]').val();
+
+        console.log('sms_mode = ' + sms_mode);
+        console.log('starttime = ' + starttime);
+        console.log('endtime = ' + endtime);
+        console.log('baseline = ' + baseline);
+
+        if (!starttime || !endtime || !baseline) {
+            alert('請記得 設定篩選器 or 使用正確的篩選值！');
+            return;
+        }
+
+        $('#filter_starttime').text( $('input[name=filter1]').val() );
+        $('#filter_endtime').text( $('input[name=filter2]').val() );
+        $('#filter_dollar').text( 'NT$' + $('select[name=filter3]').val() + ' 以上' );
+
         $.ajax({
             url:"sms_promote.php",
             method: "POST",
             dataType: "json",
-            data: {"sms_mode": $('.tab.active').attr('sms-mode')}, // sms_mode, 0 for customer, 1 for shop
+            data: {
+                "sms_mode": sms_mode,
+                "starttime": starttime,
+                "endtime": endtime,
+                "baseline": baseline
+            },
             async: false
         })
         .done(function(msg) {
@@ -37,24 +61,24 @@ $(document).ready(function(){
     });
 });
 
-function add_customer_row(phone_number, order_num, amount, sms_num) {
+function add_customer_row(phone_number, last_order, order_num, amount) {
     $('table tbody').append(
         $('<tr>').append(
             $('<td>').html("<input class=\"w3-check sms_checkbox\" type=\"checkbox\">"),
             $('<td>').text(phone_number),
+            $('<td>').text(last_order),
             $('<td>').text(order_num),
-            $('<td>').text(amount),
-            $('<td>').text(sms_num)
+            $('<td>').text(amount)
         )
     );
     return;
 }
 
-function add_shop_row(shopid, shopname, shopowner) {
+function add_shop_row(phonenumber, shopname, shopowner) {
     $('table tbody').append(
         $('<tr>').append(
             $('<td>').html("<input class=\"w3-check sms_checkbox\" type=\"checkbox\">"),
-            $('<td>').text(shopid),
+            $('<td>').text(phonenumber),
             $('<td>').text(shopname),
             $('<td>').text(shopowner)
         )
@@ -64,10 +88,12 @@ function add_shop_row(shopid, shopname, shopowner) {
 
 function handle_query(msg) {
     
+    // sms_mode is depended on selected tab
     // sms_mode, 0 for customer, 1 for shop
     var sms_mode = $('.tab.active').attr('sms-mode');
 
     if (sms_mode == 0) {
+        // add thead content
         $('table').empty().append(
             $('<thead>').append(
                 $('<tr>').append(
@@ -75,39 +101,43 @@ function handle_query(msg) {
                         '全選 <input class="w3-check sms_checkbox" type="checkbox" id="select_all">'
                     ),
                     $('<td rowspan="2">').text('號碼'),
-                    $('<td colspan="2">').text('統計報表'),
-                    $('<td rowspan="2">').text('已推播簡訊')
+                    $('<td colspan="3">').text('統計報表')
                 ),
                 $('<tr>').append(
-                    $('<td>').text('消費次數'),
+                    $('<td>').text('最後一次消費時間'),
+                    $('<td>').text('期間消費次數'),
                     $('<td>').text('累計消費金額')
                 )
             ),
             $('<tbody>')
         );
+        // add results of query into tbody
         for (var i = 0; i < msg.length; i++) {
-            add_customer_row(msg[i]['ui_phone'], msg[i]['times'], msg[i]['price'],0);
+            add_customer_row(msg[i]['ui_phone'], 0, msg[i]['times'], msg[i]['price']);
         }
     }
     else if (sms_mode == 1) {
+        // add thead content
         $('table').empty().append(
             $('<thead>').append(
                 $('<tr>').append(
                     $('<td>').append(
                         '全選 <input class="w3-check sms_checkbox" type="checkbox" id="select_all">'
                     ),
-                    $('<td>').text('分店 ID'),
+                    $('<td>').text('老闆電話'),
                     $('<td>').text('分店名稱'),
                     $('<td>').text('分店老闆')
                 )
             ),
             $('<tbody>')
         );
+        // add results of query into tbody
         for (var i = 0; i < msg.length; i++) {
-            add_shop_row(msg[i]['shop_id'], msg[i]['shop_name'], msg[i]['shop_owner']);
+            add_shop_row(msg[i]['ui_phone'], msg[i]['shop_name'], msg[i]['shop_owner']);
         }
     }
 
+    // after results put into tbody, add select_all event
     $("#select_all").on("click", function() {
         var checkstatus = this.checked;
         $('.sms_checkbox').each(function () {
