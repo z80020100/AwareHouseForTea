6da726d98c2db35343f118876760c13c4af57833
JCly-rikiu <jk123456789jp@gmail.com>
2016-11-15 15:58:09 +0800

    modify login.php to allow only customer's login

--- login.php
+++ login.php
@@ -9,9 +9,9 @@ $message = "";
 if(isset($_SESSION['u_name'])){
 	session_destroy();
 	$message = "已登出";
-	header("refresh:1;url=index.php?shop_id=" . $_shopID);
+	header("refresh:1;url=login.php?shop_id=" . $_shopID);
 }
-//$message = hash("sha256", "test1234");
+
 if(isset($_POST['submit'])){
 
 	if(!isset($_POST['password']))
@@ -20,24 +20,9 @@ if(isset($_POST['submit'])){
 	if(!isset($_POST['phone']))
 		$_POST['phone'] = '';
 
-
-	if(  user_login($_POST['username'] , $_POST['password'], $_POST['phone'], true) ){
-		if($_SESSION['shop_id'] == -1 ){
-			$message = "成功登入總店, 兩秒後自動進入總店控制台";
-			header("refresh:2;url=edit_menu.php?shop_id=" . $_SESSION['shop_id']);
-		}
-		else if($_SESSION['admin'] == 1){
-			$message = "登入成功, 兩秒後自動進入老闆控制台";
-			header("refresh:2;url=listorder.php?shop_id=" . $_SESSION['shop_id']);
-		}
-		else if ($_SESSION['staff'] == 1) {
-			$message = "登入成功, 兩秒後自動回到首頁";
-			header("refresh:2;url=index.php?shop_id=" . $_SESSION['shop_id']);
-		}
-		else{
-			$message = "登入成功, 兩秒後自動回到首頁";
-			header("refresh:2;url=index.php?shop_id=" . $_shopID);
-		}
+	if(user_login($_POST['username'] , $_POST['password'], $_POST['phone'], true)){
+		$message = "登入成功, 兩秒後自動回到首頁";
+		header("refresh:2;url=index.php?shop_id=" . $_shopID);
 	}
 	else
 	{
@@ -45,17 +30,9 @@ if(isset($_POST['submit'])){
 	}
 }
 
-if(isset($_GET['admin'] )){
-	$_HTML .= $template->render(array(
-		'ADMIN_LOGIN' => 'admin',
-		'LOGIN_MESSAGE' => $message,
-	));
-}
-else{
-	$_HTML .= $template->render(array(
-		'LOGIN_MESSAGE' => $message,
-	));
-}
+$_HTML .= $template->render(array(
+	'LOGIN_MESSAGE' => $message,
+));
 
 require_once('includes/footer.php');
 
