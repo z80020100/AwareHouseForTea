00babfe5589e243ca059b2b18ae2224e88726c21
Yist Lin <yishen992@gmail.com>
2016-11-20 01:12:58 +0800

    add permission denied redirection and refine

--- call.php
+++ call.php
@@ -1,27 +1,13 @@
 <?php
 
-//require_once('includes/general.php');
-//header("Content-Type:text/html; charset=utf-8");
-
 $_PAGE_TITLE = '上游叫料';
 require_once('includes/header.php');
 
+not_admin_redirect();
 
 $template = $twig->loadTemplate('call.html');
 
-/*
-$sql = "SELECT * FROM `orders` ORDER BY `orders`.`o_time` DESC where `orders`.`status` = WAIT";
-$result = $db->query($sql);
-$num = $db->numrow($result);
-$all_orders = array();
-*/
-
-not_admin_redirect();
-
-$_HTML .= $template->render(array(
-
-));
-
+$_HTML .= $template->render(array());
 
 require_once('includes/footer.php');
 
--- inventory.php
+++ inventory.php
@@ -1,14 +1,9 @@
 <?php
 
-require_once('includes/general.php');
-//header("Content-Type:text/html; charset=utf-8");
-
-global $db;
-
 $_PAGE_TITLE = '物料管理';
 require_once('includes/header.php');
 
-not_admin_redirect();
+not_topboss_redirect();
 
 // read data from ingredient table 
 $sql = "SELECT `shop_id`, `ingredient`, `num`, `unit`, `call_time`
@@ -23,7 +18,6 @@ $template = $twig->loadTemplate('inventory.html');
 $_HTML .= $template->render(array(
 	'data' => $result,
 ));
-
 
 require_once('includes/footer.php');
 
