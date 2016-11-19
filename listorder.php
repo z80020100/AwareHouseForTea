f4f84348aa64fe0f50878c913a5cfcdde6f1934b
JCly-rikiu <jk123456789jp@gmail.com>
2016-11-20 01:12:46 +0800

    add permission denied redirection and refine code

--- listorder.php
+++ listorder.php
@@ -1,24 +1,12 @@
 <?php
 
-// require_once('includes/general.php');
-// header("Content-Type:text/html; charset=utf-8");
-
 $_PAGE_TITLE = '即時訂單';
 require_once('includes/header.php');
 
-not_login_redirect();
 not_staff_redirect();
 
 $template = $twig->loadTemplate('listorder.html');
 
-// $sql = "SELECT * FROM `orders` WHERE `orders`.`status` = 'WAIT' ORDER BY `orders`.`o_time` DESC";
-// $result = $db->query($sql);
-// $all_orders = array();
-// if ($result) {
-// 	while ($row = $result->fetch_assoc()) {
-// 		$all_orders[] = $row;
-// 	}
-// }
 
 $_HTML .= $template->render(array(
 
