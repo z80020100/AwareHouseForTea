11d92133c3b1db2d0847b170fe3fcc492ed75c2b
Jy-Liu <b03502040@ntu.edu.tw>
2016-11-21 17:22:58 +0800

    finish top report

--- top_report.php
+++ top_report.php
@@ -7,8 +7,21 @@ not_topboss_redirect();
 
 $template = $twig->loadTemplate('top_report.html');
 
-$_HTML .= $template->render(array(
+$sql = "SELECT * FROM `shop`";
+$all_shops = array();
+foreach ($db->query($sql) as $row) {
+  $shop = array(
+    'shop_id' => $row['shop_id'],
+    'shop_name' => $row['shop_name'],
+    'shop_address' => $row['shop_address'],
+    'shop_tel' => $row['shop_tel']
+  );
 
+  $all_shops[] = $shop;
+}
+
+$_HTML .= $template->render(array(
+  'allshops' => $all_shops
 ));
 
 require_once('includes/footer.php');
